d4ceb725b48b19fabbb94485bcc55898c0cf06d0
Mathias Bjerke <mathbje@gmail.com>
2015-03-22 02:14:35 +0100

    Fix: refactores install code

--- bin/install.sh
+++ bin/install.sh
@@ -6,27 +6,39 @@ SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 #alias rm='$SCRIPT_PATH/clippy.sh "This is a really scary command. Are you sure you want to do it?"; echo -n "[yes/no]: "; read i; if [ "$i" = "yes" ]; then echo ""; ls; fi'
 
-function ls () {
-	$SCRIPT_PATH/clippy.sh "What is seen, cannot be unseen. Are you sure you want to do it?";
+
+
+#function error_handler {
+#	echo "foooo"
+#}
+#
+#trap 'error_handler' ERR 
+
+
+
+command_not_found_handle () {
+	$SCRIPT_PATH/clippy.sh "Command not found: $1 Deal with it!";
+}
+
+
+function question () {
+	$SCRIPT_PATH/clippy.sh "$1";
 
 	echo -n "[yes/no]: ";
 	read i;
 
 	if [ "$i" = "yes" ]; then
 		echo "";
-		/bin/ls "$@";
+		$2
 	fi
 }
 
 
+function ls () {
+	question "What is seen, cannot be unseen. Are you sure you want to do it?" "/bin/ls $@"
+}
+
+
 function rm () {
-	$SCRIPT_PATH/clippy.sh "This is a really scary command. Are you sure you want to do it?";
-
-	echo -n "[yes/no]: ";
-	read i;
-
-	if [ "$i" = "yes" ]; then
-		echo "";
-		/bin/rm "$@";
-	fi
+	question "This is a really scary command. Are you sure you want to do it?" "/bin/rm $@"
 }
