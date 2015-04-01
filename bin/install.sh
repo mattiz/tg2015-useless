7dfb9c1ac48d0ead25858b1622af2c11a7afc513
Mathias Bjerke <mathbje@gmail.com>
2015-04-02 00:07:12 +0200

    Add: Intro screen and custom bash prompt

--- bin/install.sh
+++ bin/install.sh
@@ -3,8 +3,16 @@
 SCRIPT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 
+export PS1="\[$(tput bold)\]\[$(tput setaf 1)\][\[$(tput setaf 3)\]Clippy Bash\[$(tput setaf 2)\]@\[$(tput setaf 4)\]\h \[$(tput setaf 5)\]\W\[$(tput setaf 1)\]]\[$(tput setaf 7)\]\\$ \[$(tput sgr0)\]"
+
+
 command_not_found_handle () {
 	$SCRIPT_PATH/clippy.sh "Command not found: $1 Deal with it!";
+}
+
+
+function say () {
+	$SCRIPT_PATH/clippy.sh "$1";
 }
 
 
@@ -17,6 +25,8 @@ function question () {
 	if [ "$i" = "yes" ]; then
 		echo "";
 		$2
+	else
+		$3
 	fi
 }
 
@@ -32,3 +42,19 @@ function rm () {
 
 
 alias cd='question "Dont get lost in the file system. Wouldent you rather go home?" "cd"'
+
+
+function assistance () {
+	clear
+	say "Great! I have just improved Bash for you. Knock yoursself out!";
+}
+
+
+function no_assistance () {
+	clear
+	say "You must have pressed the wrong key. Don't worry, I will help you anyway!";
+}
+
+
+clear
+question "Hi! I am Clippy, your bash assistant. Would you like some assistance today?" "assistance" "no_assistance"
